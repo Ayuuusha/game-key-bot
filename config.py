43894a7ec73056c101c7c14d228bb113b552df10
Ayuuusha <aisatoktobolotova@gmail.com>
2024-08-28 09:50:05 +0500

    new commit

---
+++ config.py
@@ -0,0 +1,23 @@
+
+TOKEN = '7202173894:AAGVlxJJWTQZoEaF9uH6m520o81l69rbbyk'
+
+DB_NAME = 'game_bot'
+DB_USER = 'root'
+DB_PASSWORD = 'qwerty123'
+DB_HOST = 'localhost'
+
+ADMIN_ID = 1362227483
+
+MYSQL_URL = f'mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+
+
+
+
+
+
+
+
+
+
+
